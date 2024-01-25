d6ed003299bfc094e58733933eccd9d1aadd3fb0
Daniel Eiro <daniel.eiro.01@gmail.com>
2024-01-25 15:12:39 -0300

    fix: truncating project name

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "atlas_org_id" {
 variable "atlas_project_name" {
   type        = string
   description = "Atlas Project Name"
-  default = "tech-challenge-pedidos"
+  default = "tc-pedidos"
 }
 
 # Cluster Instance Size Name 
